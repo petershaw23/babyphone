b96d6386147b93f55235077e378e6ec05dd1070a
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-02-21 20:50:11 +0100

    Update icecast.sh

--- icecast.sh
+++ icecast.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# info: this requires icecast2
+sleep 30
 ffmpeg -f alsa -acodec pcm_s24le -ac 2 -ar 44100 -i hw:1,0 -acodec libmp3lame -ab 128k -f mp3 icecast://source:tahoma@192.168.0.24:8000/baby

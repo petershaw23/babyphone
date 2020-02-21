3a972d3cc0c2432f7448508282c948559726df05
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-02-21 19:22:23 +0100

    Update rtp-baby.sh

--- rtp-baby.sh
+++ rtp-baby.sh
@@ -1,1 +1,1 @@
-arecord -f cd -D plughw:1,0 | ffmpeg -i - -acodec aac -ab 128k -ac 2 -f rtp rtp://234.5.5.5:1234
+arecord -f cd -D plughw:1,0 | ffmpeg -i - -acodec mp2 -ab 96k -ac 2 -f rtp rtp://234.5.5.5:1234

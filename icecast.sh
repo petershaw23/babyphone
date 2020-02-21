d1897ea8e13aba9775ada07b6c17937e7595152f
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-02-22 00:10:39 +0100

    Update icecast.sh

--- icecast.sh
+++ icecast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# info: this requires icecast2
+# info: this requires icecast2. stream client via http://ip:8000/baby.m3u
 sleep 30
-ffmpeg -f alsa -acodec pcm_s24le -ac 2 -ar 44100 -i hw:1,0 -acodec libmp3lame -ab 128k -f mp3 icecast://source:tahoma@192.168.0.24:8000/baby
+ffmpeg -f alsa -acodec pcm_s24le -ac 2 -ar 44100 -i hw:1,0 -acodec libopus -ab 128k -f opus -content_type application/ogg icecast://source:tahoma@192.168.0.24:8000/baby

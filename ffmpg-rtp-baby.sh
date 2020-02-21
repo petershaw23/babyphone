00fb630ce94d8bb6f3e00b2d88c0e73eb69ecfe7
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-02-21 19:31:24 +0100

    Update ffmpg-rtp-baby.sh

--- ffmpg-rtp-baby.sh
+++ ffmpg-rtp-baby.sh
@@ -1,1 +1,1 @@
-ffmpeg -f alsa -i hw:1,0 -sample_fmt S24 -acodec libmp3lame -ab 96k -f rtp rtp://234.5.5.5:1234
+ffmpeg -f alsa -i hw:1,0 -sample_fmt S32 -acodec libmp3lame -ab 96k -f rtp rtp://234.5.5.5:1234
